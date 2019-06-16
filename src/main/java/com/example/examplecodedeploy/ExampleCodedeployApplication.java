436497b95fa4d2b4b07fdcee473a69d255cb87d4
chamc1984 <chamc1984@gmail.com>
2019-06-16 11:00:12 +0900

    first commit

---
+++ src/main/java/com/example/examplecodedeploy/ExampleCodedeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.examplecodedeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleCodedeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleCodedeployApplication.class, args);
+	}
+
+}
